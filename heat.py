3fd3a6694b8076aed1afe18fb741d30832dae200
prathamdesai <prathamdesai13@gmail.com>
2018-01-02 16:16:49 -0500

    Convolve

--- heat.py
+++ heat.py
@@ -1,6 +1,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+def normalize(pix):
+
+    if pix < 0:
+
+        return 0
+
+    elif pix < 255:
+
+        return pix
+
+    else:
+
+        return 255
+
 def convolution(map, kernel, padding=1):
 
     padded_map = np.pad(map, (padding, padding), 'constant')
@@ -8,43 +22,39 @@ def convolution(map, kernel, padding=1):
     map_h, map_w, _ = padded_map.shape
     map_c = map.shape[-1]
 
-    conv_map = np.zeros(map.shape)
+    conv_map = np.zeros((map_h - k_h + 2 * padding + 1, map_w - k_w + 2 * padding + 1, map_c))
 
-    for height in range(map_h - k_h + 1):
-        for width in range(map_w - k_w + 1):
-            for channel in range(map_c):
+    for channel in range(map_c):
+        for height in range(map_h - k_h + padding):
+            for width in range(map_w - k_w + padding):
 
                 patch = padded_map[height:height + k_h, width:width + k_w, channel]
 
-                conv_map[height, width, channel] = np.sum(patch * kernel)
+                conv_map[height, width, channel] = normalize(np.sum(np.multiply(patch, kernel)))
 
     return conv_map
 
+def sharpen_kernel():
+
+    return np.array([[0, -1, 0],
+                    [-1, 5, -1],
+                    [0, -1, 0]])
+
+
+def outline_kernel():
+    return np.array([[0, -1, 0],
+                     [-1, 5, -1],
+                     [0, -1, 0]])
+
 
 if __name__ == '__main__':
-    sharpen = np.array([[0, -1, 0],
-                        [-1, 5, -1],
-                        [0, -1, 0]])
 
-    identity = np.array([[1, 0,0],
-                         [0,1,0],
-                         [0,0,1]])
+    outline = outline_kernel()
 
-    random_arr = np.array([[1,0,0],
-                           [0,1,0],
-                           [0,0,1]])
-    map = plt.imread("Maps/2.png")
+    map = plt.imread("Maps/1.png")
 
-    conv_map = convolution(map, identity)
-    print(conv_map.shape)
-    #conv_map = np.multiply(sharpen, random_arr)
-    #fig = plt.figure()
-    #ax1 = fig.add_subplot(1, 2, 1)
-    #ax1.imshow(map)
-    #ax2 = fig.add_subplot(1, 2, 2)
-    #ax2.imshow(conv_map)
+    conv_map = convolution(map, outline)
     plt.imshow(map)
     plt.show()
     plt.imshow(conv_map)
     plt.show()
-
