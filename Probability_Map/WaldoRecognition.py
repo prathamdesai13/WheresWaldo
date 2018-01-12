0739667e060ab7cea5dea22bfc772207219fb825
Jae Hoon (Antonio) Kim <antoniok9130@gmail.com>
2018-01-11 21:14:06 -0500

    Added third convolutional layer to Waldo Recognition Network.
    Trained model with 86.67% accuracy

--- Probability_Map/WaldoRecognition.py
+++ Probability_Map/WaldoRecognition.py
@@ -1,100 +1,117 @@
-from time import strftime
+from time import time, strftime
 
 import tensorflow as tf
 from numpy import array as np_array
 from numpy.random import shuffle
 
+from Probability_Map.WaldoPKL import load_waldo_pkl, load_not_waldo_pkl
+
+
 def weight_variable(shape, name):
-  initial = tf.truncated_normal(shape)
-  return tf.Variable(initial, name=name)
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial, name=name)
 
 def bias_variable(shape, name):
-  initial = tf.constant(0.1, shape=shape)
-  return tf.Variable(initial, name=name)
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial, name=name)
 
 def conv2d(x, W):
-  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
 def max_pool_2x2(x):
-  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                        strides=[1, 2, 2, 1], padding='SAME')
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                          strides=[1, 2, 2, 1], padding='SAME')
 
-class WaldoRecogonizer:
+input = tf.placeholder(tf.float32, shape=[None, 32, 32, 3], name="input")
+output = tf.placeholder(tf.float32, shape=[None, 2], name="output")
 
-    def __init__(self):
+weights1 = weight_variable([5, 5, 3, 64], "weights1")
+biases1 = bias_variable([64], "biases1")
 
-        self.input = tf.placeholder(tf.float32, shape=[32, 32, 3], name="input")
-        self.output = tf.placeholder(tf.float32, shape=[1])
+reshaped_input = tf.reshape(input, [-1, 32, 32, 3])
 
-        self.weights1 = weight_variable([5, 5, 3, 16], "weights1")
-        self.biases1 = bias_variable([16], "biases1")
+conv1 = tf.nn.relu(tf.add(conv2d(reshaped_input, weights1), biases1))
+pool1 = max_pool_2x2(conv1)
 
-        reshaped_input = tf.reshape(self.input, [1, 32, 32, 3])
+weights2 = weight_variable([5, 5, 64, 128], "weights2")
+biases2 = bias_variable([128], "biases2")
 
-        conv1 = tf.nn.relu(tf.add(conv2d(reshaped_input, self.weights1), self.biases1))
-        pool1 = max_pool_2x2(conv1)
+conv2 = tf.nn.relu(tf.add(conv2d(pool1, weights2), biases2))
+pool2 = max_pool_2x2(conv2)
 
-        self.weights2 = weight_variable([5, 5, 16, 32], "weights2")
-        self.biases2 = bias_variable([32], "biases2")
+weights3 = weight_variable([3, 3, 128, 256], "weights3")
+biases3 = bias_variable([256], "biases3")
 
-        conv2 = tf.nn.relu(tf.add(conv2d(pool1, self.weights2), self.biases2))
-        pool2 = max_pool_2x2(conv2)
+conv3 = tf.nn.relu(tf.add(conv2d(pool2, weights3), biases3))
+pool3 = max_pool_2x2(conv3)
 
-        self.fc_weights1 = weight_variable([8 * 8 * 32, 512], "fc_weights1")
-        self.fc_biases1 = bias_variable([512], "fc_biases1")
+fc_weights1 = weight_variable([4 * 4 * 256, 256], "fc_weights1")
+fc_biases1 = bias_variable([256], "fc_biases1")
 
-        h_pool2_flat = tf.reshape(pool2, [-1, 8 * 8 * 32])
-        h_fc1 = tf.nn.tanh(tf.add(tf.matmul(h_pool2_flat, self.fc_weights1), self.fc_biases1))
+h_pool3_flat = tf.reshape(pool3, [-1, 4 * 4 * 256])
+h_fc1 = tf.nn.relu(tf.add(tf.matmul(h_pool3_flat, fc_weights1), fc_biases1))
 
-        self.fc_weights2 = weight_variable([512, 1], "fc_weights2")
-        self.fc_biases2 = bias_variable([1], "fc_biases2")
+fc_weights2 = weight_variable([256, 2], "fc_weights2")
+fc_biases2 = bias_variable([2], "fc_biases2")
 
-        self.network = tf.nn.sigmoid(tf.add(tf.matmul(h_fc1, self.fc_weights2), self.fc_biases2), name="network")
+network = tf.add(tf.matmul(h_fc1, fc_weights2), fc_biases2, name="network")
 
-        self.loss = tf.reduce_mean(
-            tf.nn.softmax_cross_entropy_with_logits(labels=self.output, logits=self.network))
-        self.train_step = tf.train.AdamOptimizer(1e-6).minimize(self.loss)
-        # correct_prediction = tf.equal(self.network, self.output)
-        # accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+loss = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits(labels=output, logits=network))
+train_network = tf.train.AdamOptimizer(1e-4).minimize(loss)
 
-    def run(self, session, input):
-        output = session.run(self.network, {self.input: input})
-        return output
-
-    def train(self, session, training_data, labels=None, learning_rate=1e-2):
-
-        if labels is None:
-            for x in training_data:
-                session.run(self.train_step, feed_dict={self.input: x[0], self.output: x[1]})
-        else:
-            for x, y in zip(training_data, labels):
-                session.run(self.train_step, feed_dict={self.input: x, self.output: y})
-
+correct_prediction = tf.equal(tf.argmax(network, 1), tf.argmax(output, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), name="accuracy")
 
 if __name__ == "__main__":
 
+    # config=tf.ConfigProto(log_device_placement=True)
     with tf.Session() as session:
-        wr = WaldoRecogonizer()
-
         session.run(tf.global_variables_initializer())
 
         training_data1 = load_waldo_pkl()
-        labels1 = [np_array([1]) for _ in range(len(training_data1))]
         training_data2 = load_not_waldo_pkl()
-        labels2 = [np_array([0]) for _ in range(len(training_data2))]
-        for _ in range(1):
+        epoch = 7
+        start = time()
+        for i in range(epoch):
+            # if i%10 == 0:
+            print("Epoch:  ",i)
+            batches = [[], []]
             shuffle(training_data1)
+            for i in range(25):
+                batches[0].append(training_data1[i])
+                batches[1].append(np_array([1, 0]))
             shuffle(training_data2)
-            training_data = []
-            for i in range(50):
-                training_data.append([training_data1[i], np_array([1])])
-                training_data.append([training_data2[i], np_array([0])])
-            wr.train(session, training_data)
+            for i in range(30):
+                batches[0].append(training_data2[i])
+                batches[1].append(np_array([0, 1]))
+            session.run(train_network, feed_dict={input: batches[0], output: batches[1]})
+        end = time()
+        print("Took",(end-start),"Seconds to run",epoch,"Epochs")
 
-        saver = tf.train.Saver()
-        path = "./CNN Waldo Recognizer  {}".format(strftime("%Y-%m-%d %H_%M_%S"))
+        test_data = [[], []]
+        for data in training_data1:
+            test_data[0].append(data)
+            test_data[1].append(np_array([1, 0]))
 
-        print(saver.save(session, path))
-        test_waldo(path)
+        a1 = accuracy.eval(feed_dict={input: test_data[0], output: test_data[1]})
+        print("\nWaldo Accuracy:  ", (a1 * 100), "%")
+
+        test_data = [[], []]
+        training_data2 = load_not_waldo_pkl()
+        for data in training_data2:
+            test_data[0].append(data)
+            test_data[1].append(np_array([0, 1]))
+
+        a2 = accuracy.eval(feed_dict={input: test_data[0], output: test_data[1]})
+        print("\nNot Waldo Accuracy:  ", (a2 * 100), "%")
+
+        print("\nAverage Accuracy:  ", (a1+a2)*50, "%")
+
+        if (a1+a2)/2 > 0.9:
+            saver = tf.train.Saver()
+            path = "./CNN Waldo Recognizer___{}".format(strftime("%Y-%m-%d_%H.%M.%S"))
+
+            print(saver.save(session, path))
         # for data in training_data1:
         #     print(wr.run(session, data))
--- Probability_Map/WaldoRecognitionTest.py
+++ Probability_Map/WaldoRecognitionTest.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from numpy import array as np_array
 
 from Probability_Map.WaldoPKL import load_waldo_pkl, load_not_waldo_pkl
 
@@ -6,8 +7,6 @@ from Probability_Map.WaldoPKL import load_waldo_pkl, load_not_waldo_pkl
 def test_waldo(path):
 
     with tf.Session() as session:
-        training_data1 = load_waldo_pkl()
-        training_data2 = load_not_waldo_pkl()
 
         saver = tf.train.import_meta_graph(path+'.meta')
         saver.restore(session, tf.train.latest_checkpoint('./'))
@@ -16,18 +15,38 @@ def test_waldo(path):
         #     p.append(wr.run(session, x))
 
         graph = tf.get_default_graph()
-        network = graph.get_tensor_by_name("network:0")
 
         input = graph.get_tensor_by_name("input:0")
+        output = graph.get_tensor_by_name("output:0")
 
-        for x in training_data1:
-            print(session.run(network, {input:x}))
-        print("________________________________________________")
-        for x in training_data2:
-            print(session.run(network, {input:x}))
+        network = graph.get_tensor_by_name("network:0")
+        accuracy = graph.get_tensor_by_name("accuracy:0")
+
+        batches = [[], []]
+        training_data1 = load_waldo_pkl()
+        for data in training_data1:
+            batches[0].append(data)
+            batches[1].append(np_array([1, 0]))
+
+        a1 = accuracy.eval(feed_dict={input:batches[0], output:batches[1]})
+        print("\nWaldo Accuracy:  ",(a1*100),"%")
+        # print(network.eval(feed_dict={input:batches[0]}))
+
+
+        batches = [[], []]
+        training_data2 = load_not_waldo_pkl()
+        for data in training_data2:
+            batches[0].append(data)
+            batches[1].append(np_array([0, 1]))
+
+        a2 = accuracy.eval(feed_dict={input:batches[0], output:batches[1]})
+        print("\nNot Waldo Accuracy:  ",(a2*100),"%")
+        # print(network.eval(feed_dict={input:batches[0]}))
+
+
 
 
 
 if __name__ == "__main__":
     # train()
-    test_waldo("./Waldo Recognizer  2018-01-07 17_31_49")
+    test_waldo("CNN Waldo Recognizer___2018-01-11_20.28.13")
